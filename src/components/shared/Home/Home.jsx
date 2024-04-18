c64578672efdef0dd2872216b5cb1cb18adaa2de
zaheenrakib <rakibislambest@gmail.com>
2024-04-18 16:22:27 +0600

    Basic Setup Without FireBase

---
+++ src/components/shared/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import Footer from '../Footer/Footer';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h1>Hello Rakib</h1>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
